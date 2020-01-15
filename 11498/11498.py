d6ee198b55e58f0f34408e2e61202b5edf0f31e2
Eloy Hernandez <eloyhz@gmail.com>
2020-01-14 19:17:16 -0600

    11498 - Division of Nlogonia solved

---
+++ 11498/11498.py
@@ -0,0 +1,25 @@
+while True:
+	k = int(input())
+	if k == 0:
+		break
+	nm = input().split(' ')
+	n = int(nm[0])
+	m = int(nm[1])
+	while k > 0:
+		k = k - 1
+		xy = input().split(' ')
+		x = int(xy[0])
+		y = int(xy[1])
+		dx = x - n
+		dy = y - m
+		if dx == 0 or dy == 0:
+			print("divisa")
+		elif dx > 0 and dy > 0:
+			print("NE")
+		elif dx > 0 and dy < 0:
+			print("SE")
+		elif dx < 0 and dy < 0:
+			print("SO")
+		elif dx < 0 and dy > 0:
+			print("NO")
+
