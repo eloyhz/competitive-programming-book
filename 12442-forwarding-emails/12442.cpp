cb8e2449d70ffb160388dece59caec2c238688cc
Eloy Hernandez <eloyhz@gmail.com>
2020-05-01 23:01:16 -0500

    Forwarding Emails [TLE]

--- 12442-forwarding-emails/12442.cpp
+++ 12442-forwarding-emails/12442.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <tuple>
 #include <set>
+#include <stack>
 #include <algorithm>
 
 using namespace std;
@@ -14,12 +15,43 @@ struct info {
 };
 
 int bfs(vector<info>& data, int start)  {
+    vector<bool> visited(data.size());
+    stack<int> st;
+    int dist = 0;
+    st.push(start);
+
+    while (!st.empty()) {
+        int from = st.top();
+        st.pop();
+        if (visited[from]) {
+            continue;
+        }
+        visited[from] = true;
+        int end = data[from].to;
+        /*
+        if (data[data[from].to].distance > 0)   {
+            dist = data[data[from].to].distance;
+            if (data[data[from].to].from != start)   {
+                dist++;
+            }
+            break;
+        }
+        */
+        if (!visited[end])  {
+            st.push(end);
+            dist++;
+        }
+    }
+    data[start].distance = dist;
+    return dist;
+/*
     if (data[start].visited)    {
         return 0;
     }
     data[start].visited = true;
     int end = data[start].to;
     return bfs(data, end) + 1;
+*/
 }
 
 int main()
@@ -43,12 +75,15 @@ int main()
             if (m.distance > r) {
                 r = m.distance;
             }
-            for (auto& d : mail)    {
+/*            for (auto& d : mail)    {
                 if (d.from == -1)   {
                     continue;
                 }
                 d.visited = false;
-            }
+            }*/
+        }
+        for (auto& d : mail)    {
+            cout << "from: " << d.from << " to: " << d.to << " distance: " << d.distance << endl;
         }
         set<int> w;
         for (auto& m : mail)    {
