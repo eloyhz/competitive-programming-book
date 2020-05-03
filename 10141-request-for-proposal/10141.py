b2f89e0667d0d7bdacd1b69301e39bc945754f04
Eloy Hernandez <eloyhz@gmail.com>
2020-05-02 19:00:21 -0500

    Request for proposal [AC]

--- 10141-request-for-proposal/10141.py
+++ 10141-request-for-proposal/10141.py
@@ -1,6 +1,6 @@
 # https://onlinejudge.org/external/101/10141.pdf
 
-from sys import stdin
+from sys import stdin, maxsize
 
 rfp = 0
 while True:
@@ -8,6 +8,8 @@ while True:
     if n == 0 and p == 0:
         break
     rfp += 1
+    if rfp > 1:
+        print()
     low_proposal = ""
     max_compliance = 0
     proposals = {}
@@ -18,17 +20,24 @@ while True:
         dr = stdin.readline().split()
         d = float(dr[0])
         r = int(dr[1])
-        for i in range(r):
+        for _ in range(r):
             stdin.readline()
         compliance = r / n
         if compliance > max_compliance:
             max_compliance = compliance
             low_proposal = proposal
         if compliance not in proposals:
-            proposals[compliance] = set()
-        proposals[compliance].add((i, proposal, d))
+            proposals[compliance] = []
+        proposals[compliance].append((i, proposal, d))
     print("RFP #", rfp, sep="")
-    print("--------------")
-    print(len(proposals[max_compliance]))
-    print(proposals[max_compliance])
-    print(low_proposal)
+    # print("--------------")
+    # print(len(proposals[max_compliance]))
+    # print(proposals[max_compliance])
+    if len(proposals[max_compliance]) > 1:
+        lowest_price = maxsize
+        for s in proposals[max_compliance]:
+            if s[2] < lowest_price:
+                lowest_price = s[2]
+                low_proposal = s[1]
+    # print(lowest_price)
+    print(low_proposal, end="")
